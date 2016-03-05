f40101eaeaf958549c3d3099331f5c9f02f4e5d1
zhuotao25 <zhuotaohuang@umass.edu>
2016-03-04 23:32:36 -0500

    finished pagerank algorithm

--- src/pagerank/PageRank.java
+++ src/pagerank/PageRank.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -19,27 +19,40 @@ import java.util.Map;
 
 public class PageRank {
 
-	private static HashMap<String, ArrayList<String>> createGraph(){
-		HashMap<String, ArrayList<String>> vertices=new HashMap<String, ArrayList<String>>();
-		String fileName = "links.srt";
-		String line = null;
+	private final static double LAMBDA=0.15;
+	private final static double TAU=0.01;
+
+	// read the file and save the information of all the pages and their incoming links and outgoing links
+	private static HashMap<String, ArrayList<String>> createGraph(HashMap<String, ArrayList<String>> pagesWithInlinks, HashMap<String, ArrayList<String>> pagesWithOutlinks ){
+		String fileName = "links.srt"; // file to read
+		String line = null; 
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-			while((line = bufferedReader.readLine()) != null) {
-				//System.out.println(line);
-				String[] edge=line.split("\t");
-				if(edge.length==2){
-					String v1=edge[0];
-					String v2=edge[1];
-					ArrayList<String> incomingLinks;
-					if(!vertices.containsKey(v2)){
-						incomingLinks=new ArrayList<String>();
+			while((line = bufferedReader.readLine()) != null) { // read new line
+				String[] edge=line.split("\t"); // split string into array 
+				if(edge.length==2){ // if the string contains two words
+					String src=edge[0];
+					String dest=edge[1];
+
+					ArrayList<String> incomingLinks; // list for incoming links
+					if(!pagesWithInlinks.containsKey(dest)){ // if page is not in the hashmap
+						incomingLinks=new ArrayList<String>(); // create a new list
 					}
-					else{
-						incomingLinks=vertices.get(v2);
+					else{ // if page is in the hashmap
+						incomingLinks=pagesWithInlinks.get(dest); // get the list
 					}
-					incomingLinks.add(v1);
-					vertices.put(v2, incomingLinks);
+					incomingLinks.add(src); // add the new src page to list
+					pagesWithInlinks.put(dest, incomingLinks); // add the dest and its list of incoming links to hashmap
+
+					ArrayList<String> outgoingLinks; // list of outgoing links
+					if(!pagesWithOutlinks.containsKey(src)){ // if page is not in the hashmap
+						outgoingLinks=new ArrayList<String>(); // create a new list
+					}
+					else{ // if page is in the hashmap
+						outgoingLinks=pagesWithOutlinks.get(src); // get the list
+					}
+					outgoingLinks.add(dest); // add the new dest page to list
+					pagesWithOutlinks.put(src, outgoingLinks); // add the src and its list of outgoing links to hashmap
 				}
 				else{
 					System.out.println("Read an invalid line");
@@ -48,21 +61,33 @@ public class PageRank {
 			bufferedReader.close();         
 		}
 		catch(FileNotFoundException ex) {
-			System.out.println( "Unable to open file: " +fileName);                
+			System.out.println("Unable to open file: "+fileName);                
 		}
 		catch(IOException ex) {
-			System.out.println( "Error reading file: " +fileName); 
+			System.out.println("Error reading file: "+fileName); 
 		}
-		System.out.println("**Done**");
-		System.out.println("Size: "+ vertices.size());
-		return vertices;
+		return pagesWithInlinks;
 
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked", "resource" })
+	// combine the pages with incoming links and pages with outgoing links to get all pages
+	private static HashSet<String> getAllPages(HashMap<String, ArrayList<String>> in, HashMap<String, ArrayList<String>> out){
+		HashSet<String> all=new HashSet<String>();
+		for (String s : out.keySet()) {
+			all.add(s);
+		}
+		for (String s : in.keySet()) {
+			all.add(s);
+		}
+		return all;
+	}
+	
+
+	// write the first 50 pages with the most incoming links
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private static void inlinks(HashMap m) throws IOException{
-		List entriesList = new LinkedList<ArrayList<String>>(m.entrySet());
-		Collections.sort(entriesList, new Comparator(){
+		List entriesList = new LinkedList<ArrayList<String>>(m.entrySet()); //cover hashmap to list
+		Collections.sort(entriesList, new Comparator(){ //use Comparator to sort the list
 			public int compare(Object a, Object b) {
 				return ((Comparable) ((ArrayList<String>) ((Map.Entry) (b)).getValue()).size())
 						.compareTo(((ArrayList<String>) ((Map.Entry) (a)).getValue()).size());
@@ -70,28 +95,121 @@ public class PageRank {
 		});
 		Iterator it = entriesList.iterator();
 		int count=1;
-		String fileName = "inlinks.txt";
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+		String fileName = "inlinks.txt"; // file to write
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName)); // create writer
 		while(it.hasNext()) {
 			Map.Entry entry = (Map.Entry) it.next();
-			
-			if(count<=50){ // print the first 50 pages
-				System.out.println(count+" *** "+entry.getKey()+" *** "+((ArrayList<String>) entry.getValue()).size());
-				bufferedWriter.write(entry.getKey()+"\t"+((ArrayList<String>) entry.getValue()).size());
+			if(count<=50){ // write the first 50 pages
+				//System.out.println(count+" *** "+entry.getKey()+" *** "+((ArrayList<String>) entry.getValue()).size());
+				bufferedWriter.write(entry.getKey()+"\t"+count+"\t"+((ArrayList<String>) entry.getValue()).size());
 				bufferedWriter.newLine();
 			}
 			count++;
 		}
 		bufferedWriter.close();
 	}
+	
+	// a method to create a hashmap with key:String and value:Double
+	private static HashMap<String,Double> createHashMap(HashSet<String> s, double v){
+		HashMap<String, Double> m=new HashMap<String, Double>();
+		for(String key : s)
+		{
+			m.put(key, v);
+		}
+		return m;
+	}
 
-	public static void main(String arg[]) throws IOException{
-		long lStartTime = new Date().getTime();
+	// calculate the pagerank
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static LinkedHashMap<String, Double> rank(HashMap<String, ArrayList<String>> out, HashSet all) throws InterruptedException{
+		ArrayList allPages=new ArrayList(all); // create a list of pages
+		int numPages=all.size(); // number of all pages |P|
+		HashMap iPageRank=createHashMap(all,1.0/numPages);// this is I (current pagerank estimate) and initial value is 1/|P|
+		HashMap rPageRank=createHashMap(all,LAMBDA/numPages); // this is R (resulting better pagerank estimate)
+		double convergence=1; // set convergence to 1
+		while(convergence>TAU){ // if convergence > tau
+			for(int i=0;i<numPages;i++){
+				String s=(String) allPages.get(i);
+				rPageRank.put(s, LAMBDA/numPages); // set all R to 1/|P|
+			}
+			for(int i=0;i<numPages;i++){ // for all pages
+				String page=(String) allPages.get(i);
+				if(out.containsKey(page)){ // if current page has outgoing links
+					ArrayList myOutPages=out.get(page);
+					for(int j=0;j<myOutPages.size();j++){ // for each outgoing link
+						String f=(String) myOutPages.get(j);
+						// recalculate the pagerank
+						rPageRank.put(f, (double)rPageRank.get(f)+(1-LAMBDA)*(double)iPageRank.get(page)/(double)myOutPages.size());
+					}
+				}// else if current page has outgoing links, we do nothing for now and will handle it at the end
+			}
+			
+			// calculate convergence
+			double sumOfDiff=0;
+			for(int k=0;k<numPages;k++){
+				String currPage=(String) allPages.get(k);
+				double r=(double)rPageRank.get(currPage);
+				double d=Math.pow((double)iPageRank.get(currPage)-r, 2);// (i-r)^2
+				sumOfDiff+=d; // sum all (i-r)^2
+				iPageRank.put(currPage, r); // update the pagerank
+			}
+			convergence=Math.sqrt(sumOfDiff); // calculate convergence
+			//System.out.println("convergence: "+convergence);
+		} // end while 
+		
+		double sumAllPR=0; // sum of all pageranks
+		for(int k=0;k<numPages;k++){
+			String currPage=(String) allPages.get(k);
+			sumAllPR+=(double)iPageRank.get(currPage); // sum up all pagerank
+		}
+		
+		// update page rank
+		for(int g=0;g<numPages;g++){
+			String curPage=(String) allPages.get(g);
+			iPageRank.put(curPage, (double)iPageRank.get(curPage)/sumAllPR); //scale the pagerank so that they will sum up to 1
+		}
+		
+		//sort the pages by pagerank
+		List entriesList = new LinkedList(iPageRank.entrySet()); // cover hashmap to list
+		Collections.sort(entriesList, new Comparator(){ // use Collection.sort to sort
+			public int compare(Object a, Object b) {
+				return ((Comparable) ((Map.Entry) (b)).getValue()).compareTo(((Map.Entry) (a)).getValue());
+			}
+		});
+		LinkedHashMap sortedPages = new LinkedHashMap(); // create a linked hashmap
+		Iterator it = entriesList.iterator();
+		while(it.hasNext()) {
+			Map.Entry entry = (Map.Entry) it.next();
+			sortedPages.put(entry.getKey(), entry.getValue()); // put the sorted pages to the linked hashmap
+		}
+		return sortedPages;
+	}
 
-		inlinks(createGraph());
-
-		long lEndTime = new Date().getTime();
-		long difference = lEndTime - lStartTime;
-		System.out.println("Elapsed seconds: " + difference/1000+"s");
+	public static void main(String arg[]) throws IOException, InterruptedException{
+		HashMap<String, ArrayList<String>> in=new HashMap<String, ArrayList<String>>();
+		HashMap<String, ArrayList<String>> out=new HashMap<String, ArrayList<String>>();
+		
+		//////////////Part A/////////////////
+		createGraph(in,out); // store information about pages, incoming links and outgoing links
+		inlinks(in); // PartA: write the first 50 pages with most incoming links to the file
+		
+		//////////////Part B//////////////////
+		HashSet<String> allPages=getAllPages(in,out);
+		LinkedHashMap<String,Double> gg=rank(out,allPages);// calculate page rank
+		String fileName = "pagerank.txt";
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+		int count=0;
+		//  write the first 50 pages with the highest page rank to the file
+		for(Map.Entry<String, Double> entry : gg.entrySet()){ 
+			String s=entry.getKey()+"\t"+(count+1)+"\t"+entry.getValue();
+			//System.out.printf("%s\t%s%n", entry.getKey(), entry.getValue()); 
+			bufferedWriter.write(s);
+			count++;
+			if(count>=50){
+				break;
+			}
+			bufferedWriter.newLine();
+		}
+		bufferedWriter.close();
 	}
 }
